a847e4a669eb4580707d704d3db017b55a028d85
Venu <Venu@DESKTOP-6LLIN5D>
2020-03-31 10:14:44 +0530

    after commit

--- src/iconnect/hhcl/invoker/IconnectListener.java
+++ src/iconnect/hhcl/invoker/IconnectListener.java
@@ -4,7 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-public class OrganogramListener implements HttpSessionListener{  
+public class IconnectListener implements HttpSessionListener{  
 	ServletContext ctx=null;  
 	static int total=0,current=0;  
 	public void sessionCreated(HttpSessionEvent e) {  
